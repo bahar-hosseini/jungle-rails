1e72f2bd16b8bbcab9534d8afe16cb03af4e48e6
vagrant <bahar.hosseini@gmail.com>
2022-09-23 22:06:38 +0000

    Use rubocop to style checking and code formatting && Add order detailes

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
-class Admin::DashboardController < ApplicationController
-  def show
+# frozen_string_literal: true
+
+module Admin
+  class DashboardController < ApplicationController
+    def show; end
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,9 @@
+# frozen_string_literal: true
+
 class Order < ApplicationRecord
-  
   has_many :line_items
 
   monetize :total_cents, numericality: true
 
   validates :stripe_charge_id, presence: true
-
 end
